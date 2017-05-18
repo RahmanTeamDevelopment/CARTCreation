1c8cba1ce2f1c641fb0eb9d714cbbee53b3ba30e
Márton Münz <Marton.Muenz@icr.ac.uk>
2017-05-18 11:22:38 +0200

    Empty unit tests added for the core module

---
+++ test/test_core.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+from cartcreation import core
+
+class TestTranscript(TestCase):
+
+    def test_read(self):
+        self.fail()
+
+    def test_split_exons(self):
+        self.fail()
+
+    def test_utr5_start(self):
+        self.fail()
+
+    def test_utr3_end(self):
+        self.fail()
+
+    def test_UTR_ends(self):
+        self.fail()
+
+    def test_utr5_exonic_content_length(self):
+        self.fail()
+
+    def test_utr3_exonic_content_length(self):
+        self.fail()
