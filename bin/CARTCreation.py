2673a4f3942a68c26df768286cf3916252924ae4
Márton Münz <Marton.Muenz@icr.ac.uk>
2017-06-02 19:26:43 +0200

    Minor changes in the interface

--- bin/CARTCreation.py
+++ bin/CARTCreation.py
@@ -10,11 +10,11 @@ ver = '0.1.0'
 
 # Command line argument parsing
 descr = 'CARTCreation tool'
-parser = OptionParser(usage='python cart_creation <options>', version=ver, description=descr)
-parser.add_option("--input", dest='input', action='store', help="Input file")
-parser.add_option("--refsdb", dest='refsdb', action='store', help="RefSeq transcript database file")
-parser.add_option("--refss", dest='refss', action='store', help="refseq_scan output file")
-parser.add_option("--out", dest='out', action='store', help="Output file prefix")
+parser = OptionParser(version=ver, description=descr)
+parser.add_option("-i", dest='input', action='store', help="Input file")
+parser.add_option("-d", dest='refsdb', action='store', help="RefSeq transcript database file")
+parser.add_option("-s", dest='refss', action='store', help="refseq_scan output file")
+parser.add_option("-o", dest='out', action='store', help="Output file prefix")
 (options, args) = parser.parse_args()
 
 
