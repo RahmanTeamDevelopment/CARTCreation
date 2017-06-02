48608dce5882cb4a40c97c711af82c9ca376d8a0
Márton Münz <Marton.Muenz@icr.ac.uk>
2017-06-02 19:41:54 +0200

    Minor changes in the interface

--- bin/CARTCreation.py
+++ bin/CARTCreation.py
@@ -9,14 +9,13 @@ import datetime
 ver = '0.1.0'
 
 # Command line argument parsing
-descr = 'CARTCreation tool'
-parser = OptionParser(version=ver, description=descr)
+descr = 'CARTCreation v'+ver
+parser = OptionParser(usage='CARTCreation/env/bin/cart_creation <options>', version=ver, description=descr)
 parser.add_option("-i", dest='input', action='store', help="Input file")
 parser.add_option("-d", dest='refsdb', action='store', help="RefSeq transcript database file")
 parser.add_option("-s", dest='refss', action='store', help="refseq_scan output file")
 parser.add_option("-o", dest='out', action='store', help="Output file prefix")
 (options, args) = parser.parse_args()
-
 
 print '\n'+'='*100
 print 'CARTCreation v'+ver+' started: '+str(datetime.datetime.now())+'\n'
